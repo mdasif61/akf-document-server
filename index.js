fc4292d48cc8c0913d70fcce9f0f93e118b2d446
Ikbal Hossain Asif <asifikbalgfh312@gmail.com>
2024-02-24 23:52:30 +0600

    add corsConfig

--- index.js
+++ index.js
@@ -7,8 +7,13 @@ require('dotenv').config();
 
 connectDB()
 const app=express();
+const corsConfig = {
+    origin: '',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
 
-app.use(cors());
+app.use(cors(corsConfig));
 app.use(express.json());
 
 // app.get('/',(req,res)=>{
